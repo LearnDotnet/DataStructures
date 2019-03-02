42ef6239fc2d03c493f8cf80775924cbc1e85420
hkk2016 <kerahari@gmail.com>
2019-03-02 17:39:28 +0530

    Basic working BST  Implememtation

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -10,6 +10,29 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            BinarySearchTree BST = new BinarySearchTree();
+            BSTNode root = null;
+            root = BST.Insert(root, 10);
+            root = BST.Insert(root, 5);
+            root = BST.Insert(root, 20);
+
+            root = BST.Insert(root, 7);
+            root = BST.Insert(root, 15);
+            root = BST.Insert(root, 12);
+
+            BST.PrintDFS_PreOrder(root);
+
+            Console.WriteLine("Max={0},Min={1}", BST.FindMax(root), BST.FindMin(root));
+            Console.WriteLine("IsExists({0})={1}", 15, BST.IsExists(root,15));
+            Console.WriteLine("IsExists({0})={1}", 10, BST.IsExists(root, 10));
+            Console.WriteLine("IsExists({0})={1}", 5, BST.IsExists(root, 5));
+            Console.WriteLine("IsExists({0})={1}", 12, BST.IsExists(root, 12));
+
+            Console.WriteLine("IsExists({0})={1}", 8, BST.IsExists(root, 8));
+            Console.WriteLine("IsExists({0})={1}", 6, BST.IsExists(root, 6));
+
+            BST.PrintBFS(root);
+            Console.ReadLine();
         }
     }
 }
